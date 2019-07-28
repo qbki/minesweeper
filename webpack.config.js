4dd33bc629c18770bb9c24dbab789e8b796841b5
Sergey Andreev <andrejew.sw@gmail.com>
2019-07-28 21:30:19 +0300

    Styles modules ware removed

--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,6 @@ module.exports = {
           }
         ],
       },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      }
     ],
   },
   resolve: {
